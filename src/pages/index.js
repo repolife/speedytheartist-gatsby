4bc6bd5a36e7f7f9443d8864187e920869509001
David Vargas <getdavidvargas@gmail.com>
2022-09-22 15:48:25 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,68 +2,25 @@ import React, { useState } from "react"
 import {
   GlobalStyle,
   Container,
-  BackgroundImage,
-  ModelContainer,
-  ModelImage,
-  ModelLink,
-  SideTab,
-  Menu,
+  Left
+
 } from "../style/base"
-import { FaTimesCircle } from "react-icons/fa"
-import Platform from "../components/platform"
-import SocialMedia from "../components/socialmedia"
-import Helmet from "react-helmet"
-import bottomLeft from "../images/Back bottom left.png"
-import { motion } from "framer-motion"
-import { Link } from "gatsby"
+
+
+
 
 const Home = () => {
-  const [toggle, setToggle] = useState({ display: false })
-  const [stone, clickStone] = useState({ click: false })
-  const { display } = toggle
 
-  const toggleDisplay = () => {
-    setToggle({ display: !toggle.display })
-  }
-  const variants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-  }
 
   return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      variants={variants}
-      transition={{ duration: 2 }}
-    >
-      <Container>
-        <GlobalStyle />
 
-        <ModelLink>
-          <motion.div
-            whileHover={{ opacity: 0.2 }}
-            whileTap={{ opacity: 0.2 }}
-            style={{ opacity: 1 }}
-          >
-            <BackgroundImage></BackgroundImage>
-          </motion.div>
-        </ModelLink>
-        <motion.div
-          whileHover={{ scale: 0.8 }}
-          whileTap={{ scale: 0.8 }}
-          animate={{ rotate: 360 }}
-          transition={{ duration: 2 }}
-        >
-          <Link to="/mylinks" title="My Links">
-            <SideTab title="View Menu">
-              {" "}
-              <img src={bottomLeft}></img>
-            </SideTab>
-          </Link>
-        </motion.div>
-      </Container>
-    </motion.div>
+<Container>
+<GlobalStyle />
+
+  <Left>hi</Left>
+  <h2>hi</h2>
+</Container>
+
   )
 }
 
--- src/style/base.js
+++ src/style/base.js
@@ -7,158 +7,50 @@ import bigBackground from "../images/bigBackgroundpg.jpg"
 import frontRight from "../images/Front top right.png"
 import lowResImage from "../images/BigBackgroundpglowres.jpg"
 
-const gridPadding = {
-  padding: "10px",
-}
-
-const pallet = { red: `#bf504c` }
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Raleway&display=swap')
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Oswald:400,500,600,700')
 
 * {
-    margin: 0;
-    padding: 0;
-}
-
-*,
-*::before,
-*::after {
-    box-sizing: inherit;
-}
-    
-html {
-    box-sizing: border-box;
-    font-size: 62.5%; 
-    
-    @media only screen and (max-width: $-large) {
-        font-size: 50%;
-    }
-}
-
-body {
-    font-family: 'Raleway', sans-serif;
-    font-weight: 400;
-    line-height: 1.6;
-    min-height: 100vh;
-    background: #212725;
-   }     
+  box-sizing: border-box;
+  text-decoration: none;
+}  
  `
 const Container = styled.div`
-  width: 100%;
-  margin: 0 auto;
-`
-
-const ModelContainer = styled.div`
-  max-width: 100vw;
-  height: 100vh;
-  object-fit: cover;
-  object-position: top left;
-  position: relative;
-`
-
-const ModelLink = styled.a`
-  a:hover {
-    color: red;
-  }
-`
-
-const ModelImage = styled.img`
-  background: url(${frontRight}) no-repeat center;
-  display: flex;
-  width: 100%;
-  z-index: 1;
-
-  flex-direction: row;
-  align-content: center;
-  justify-content: center;
-  position: fixed;
-  bottom: 25vh;
-`
-
-const BackgroundImage = styled.div`
-  background: url(${lowResImage}) no-repeat center;
-  height: 100vh;
   width: 100vw;
-  object-fit: cover;
-  object-position: center;
-  position: fixed;
-  background-size: cover;
+  heigh: 100vh;
+  positon: relative;
 `
+const Left = styled.div`
+  width: 30vw;
+  height: 100vh;
+  float: left;
+  background: red;
+  `
 
-const PlatformLinks = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 1.5rem;
+  const Logo = styled.div` 
+    margin: 30px 0 0 30px;  `
 
-  & > a {
-    background: ${pallet.red};
-    margin: 1em;
-    padding: 1em;
-    min-width: 25vh;
-    max-width: 25vh;
-    text-align: center;
-    border-radius: 10px;
-    color: white;
-    text-decoration-line: none;
-    &: hover {
-      box-shadow: 2px 2px 2px 2px #f7f7f73b;
-    }
-  }
+
+
+const Menu = styled.div`
+margin-top: 150px;
+width: 30%;`
+
+const Links = styled.div`
+margin-bottom: 10px;
+a {
+  font-size: 12px;
+  color: #fff;
+  font-family: oswald;
+  float: right;
+}
+
 `
-const SocialMediaLinks = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  font-size: 1.5rem;
-  align-items: center;
-  & > a {
-    width: 4em;
-    margin: 1em;
-    color: white;
-    display: flex;
-    flex-direction: column;
-    text-decoration: none;
-    align-items: center;
-  }
-`
-
-const MusicLink = styled.a`
-  display: flex;
-`
-const SideTab = styled.a`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  font-size: 5em;
-  z-index: 2;
-  cursor: pointer;
-
-  & > img {
-    height: 50vh;
-  }
-`
-
-const CloseButton = styled.div`
-  color: ${pallet.red};
-  cursor: pointer;
-  width: min-content;
-`
-
-const Menu = styled.div``
 export {
   GlobalStyle,
   Container,
-  gridPadding,
-  BackgroundImage,
-  ModelContainer,
-  ModelImage,
-  ModelLink,
-  PlatformLinks,
-  SocialMediaLinks,
-  SideTab,
-  CloseButton,
+  Left,
+
   Menu,
 }
