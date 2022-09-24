f35a90b0ad40ad88d64edc78acb0db258bfc83d3
David Vargas <getdavidvargas@gmail.com>
2022-09-24 15:30:27 -0500

    Add social link component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { SiLinktree } from 'react-icons/si'
-import {FaTwitter, FaInstagram, FaYoutube, FaFacebook, FaTiktok} from 'react-icons/fa'
-import {Social, Footer as FooterContiner} from "../style/base"
+import {Footer as FooterContiner} from "../style/base"
+import { SocialLink } from './Social/SocialLink';
+
+
 export const Footer = () => {
 
     return(<FooterContiner>
-        <Social><FaTwitter size={20} color='white' /></Social>
-        <Social><FaInstagram size={20} color='white' /></Social>
-        <Social><SiLinktree size={20} color='white' /></Social>
-        <Social><FaYoutube size={20} color='white' /></Social>
-        <Social><FaFacebook size={20} color='white' /></Social>
-        <Social><FaTiktok size={20} color='white' /></Social>
+    <SocialLink name="twitter" props={{size:20, color:'green' }}/>
 
     </FooterContiner>)
      
