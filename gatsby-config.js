f87319cd6ff379ad47771bfca0e9c4dc662f7948
David Vargas <getdavidvargas@gmail.com>
2022-09-26 00:39:02 -0500

    Fix build issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,39 +12,8 @@ module.exports = {
         background_color: `black`,
         theme_color: `#6B5211`,
         display: `standalone`,
-        icon: `src/images/model.png`,
+        icon: `src/assets/images/logo.png`,
       },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Oswald`,
-            variants: [`400`, `500`, `600`, `700`],
-          },
-          {
-            family: `Open Sans`,
-            variants: [`400`, `500`, `600`, `700`]
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-alias-imports`,
-      options: {
-        alias: {
-          "@components": path.resolve(__dirname, './src/componets/'),
-          "@src": path.resolve(__dirname, './src/'),
-
-          "@images": path.resolve(__dirname, './src/assets/images/'),
-          extensions: [
-            "js", "jsx", "png", "jpg"
-
-          ],
-        },
-
-      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
