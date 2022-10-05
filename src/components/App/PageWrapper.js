f297c7f41de248312848a419273ab1265e947f44
David Vargas <getdavidvargas@gmail.com>
2022-10-05 09:02:32 -0500

    Add logic for getting spotify albums

--- src/components/App/PageWrapper.js
+++ src/components/App/PageWrapper.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import axios from 'axios'
 
 import { Nav } from '../Nav/Nav'
@@ -7,10 +7,12 @@ import { MusicContext } from '../../context/MusicContext'
 import { useStaticQuery, graphql } from 'gatsby'
 
 export const PageWrapper = ({ children }) => {
-    const [music, setMusic] = useState(null)
-    const [refreshToken, setREfreshToken] = useState(null)
+    const [music, setMusic] = useState([])
+    const [refreshToken, setRefreshToken] = useState('')
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(null)
+
+    const { GATSBY_SPOTIFY_ARTIST_ID: artistId } = process.env
 
     const data = useStaticQuery(graphql`
         {
@@ -33,34 +35,91 @@ export const PageWrapper = ({ children }) => {
         }
     `)
 
-    const getRefreshToken = async () => {
-        try {
-            const response = await axios.get(`/.netlify/functions/spotify`)
-            const token = response.data.data.access_token
-            setREfreshToken(token)
-            localStorage.setItem('token', token)
-            setLoading(false)
-        } catch (error) {
-            setError(true)
+    // useEffect(() => {
+    //     const getRefreshToken = async () => {
+    //         try {
+    //             const response = await axios.get(`/.netlify/functions/spotify`)
+    //             const token = await response.data.data.access_token
+    //             return await token
+    //         } catch (error) {
+    //             setError(true)
+    //         }
+    //     }
+
+    //     getRefreshToken().then(token => setRefreshToken(token))
+    // }, [])
+
+    useEffect(() => {
+        if (!refreshToken) return
+
+        const getSpotfyAlbums = async token => {
+            try {
+                const response = await axios.get(
+                    `https://api.spotify.com/v1/artists/${artistId}/albums`,
+
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            'Content-type': 'application/json',
+                        },
+                        params: {
+                            limit: 20,
+                            offset: '1',
+                        },
+                    }
+                )
+                setMusic(response.data.items)
+            } catch (error) {
+                setError(error)
+            }
         }
-    }
 
-    useEffect(() => {
-        if (localStorage.getItem('token')) return
-        setLoading(true)
-        getRefreshToken()
-    }, [])
-
-    useEffect(() => {
-        if (refreshToken) return
-        setMusic(data)
+        getSpotfyAlbums(refreshToken)
     }, [refreshToken])
+
+    useEffect(() => {
+        if (refreshToken !== '') return
+        setMusic(data.allFile.edges)
+    }, [refreshToken])
+
+    const preparedSpotifyItems = useMemo(() => {
+        if (music.length < 0) return
+        if (refreshToken === '') return
+
+        const items = []
+
+        console.log(music)
+
+        music.map(item =>
+            items.push({
+                id: item.id,
+                title: item.name,
+                artwork: item.images[0].url,
+                url: item.external_urls.spotify,
+            })
+        )
+    }, [music])
+
+    const preparedDefaultMusic = useMemo(() => {
+        if (music.length < 0) return
+        if (refreshToken !== '') return
+
+        const items = []
+
+        const { id } = music[0].node
+
+        items.push({
+            id,
+        })
+
+        console.log('yup', items)
+    }, [music])
 
     return (
         <Container>
             <GlobalStyle />
             <Nav />
-            <MusicContext.Provider value={{ music }}>
+            <MusicContext.Provider value={{ music: preparedSpotifyItems }}>
                 {children}
             </MusicContext.Provider>
         </Container>
--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -4,18 +4,18 @@ import MusicItem from '.'
 import { FaSpotify } from 'react-icons/fa'
 
 export const Music = ({ title, image, url }) => {
-    const { music } = useContext(MusicContext)
+    // const { music } = useContext(MusicContext)
 
-    const defaultNode =
-        music?.allFile.edges[0].node.childMarkdownRemark.frontmatter
+    // const defaultNode =
+    //     music?.allFile.edges[0].node.childMarkdownRemark.frontmatter
 
-    if (!defaultNode) {
-        return <h2>loading...</h2>
-    }
+    // if (!defaultNode) {
+    //     return <h2>loading...</h2>
+    // }
 
     return (
         <MusicItem>
-            <MusicItem.Title>{defaultNode.title.toUpperCase()}</MusicItem.Title>
+            {/* <MusicItem.Title>{defaultNode.title.toUpperCase()}</MusicItem.Title>
             <MusicItem.ArtWork src={defaultNode.image.publicURL} />
 
             <MusicItem.Listen>
@@ -24,7 +24,7 @@ export const Music = ({ title, image, url }) => {
                     {' '}
                     <FaSpotify color="green" />
                 </MusicItem.Link>
-            </MusicItem.Listen>
+            </MusicItem.Listen> */}
         </MusicItem>
     )
 }
