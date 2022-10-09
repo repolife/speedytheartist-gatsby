07110dbdb4d89c866cc561b3ae1071a35dd16823
David Vargas <getdavidvargas@gmail.com>
2022-10-08 22:33:56 -0500

    Add links to bottom

--- src/components/Nav/LinkContainer.jsx
+++ src/components/Nav/LinkContainer.jsx
@@ -4,12 +4,15 @@ import { useAnimationControls, motion } from 'framer-motion'
 import { useActive } from '../../hooks/useActive'
 
 const Container = styled(motion.div)`
-    margin-bottom: 10px;
+    margin-bottom: 1.2rem;
     display: flex;
     flex-direction: row;
     width: 15vw;
     justify-content: space-evenly;
     align-items: center;
+    @media screen and (max-width: 650px) {
+        margin-bottom: 2rem;
+    }
 `
 
 export const LinkContainer = ({ isActive, children }) => {
